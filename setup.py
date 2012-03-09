1874f67571397b8ece212c22613b268ce9f0a655
Gabor Guzmics <gab@g4b.org>
2012-03-09 13:16:25 +0100

    setup.

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sitenode',
+    version='0.99',
+    description='Basic Models for Web Development',
+    author='g4b',
+    author_email='gab(at)g4b.org',
+    url='',
+    download_url='',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GPL License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ]
+)
