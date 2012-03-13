22636261726554ec5b81754fab555a25041b8fce
Gabor Guzmics <gab@g4b.org>
2012-03-13 16:27:41 +0100

    waaaah

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sitenode',
-    version='0.99.5',
+    version='0.99.6',
     description='Basic Models for Web Development',
     author='g4b',
     author_email='gab(at)g4b.org',
@@ -13,7 +13,7 @@ setup(
     #requires=['django-tinymce', 'django-endless-pagination',
     #                  'django-grappelli', 'django-filebrowser>=3.4.0'],
     package_dir={'': 'src'},
-    packages=find_packages(),
+    packages=find_packages('src/'),
     include_package_data=True,
     zip_safe=False,
     classifiers=[
