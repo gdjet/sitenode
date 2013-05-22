7c046284f55dae4e33af6a4e9a2e33c7d4f63bc9
Gabor Guzmics <gab@g4b.org>
2013-05-22 14:57:06 +0200

    version change

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sitenode',
-    version='0.99.9',
+    version='0.99.10',
     description='Basic Models for Web Development',
     author='g4b',
     author_email='gab(at)g4b.org',
